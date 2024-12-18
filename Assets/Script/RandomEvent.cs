09ec6cc2738612d7abb506ba0867ee54b50defe7
Alban <dreamsky0ytb@gmail.com>
2024-12-18 10:51:04 +0100

    Travail boss

---
+++ Assets/Script/RandomEvent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RandomEvent : MonoBehaviour
+{
+    int NumberOne = 0;
+    public int NumberAdd = 1;
+}
